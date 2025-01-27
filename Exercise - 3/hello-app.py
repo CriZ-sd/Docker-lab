f64ee8bb5a04691a78d4aeecc37b9ab6fcd1e98e
Christo Sad <74500978+CriZ-sd@users.noreply.github.com>
2025-01-27 22:28:54 +0200

    Update hello-app.py

--- Exercise - 3/hello-app.py
+++ Exercise - 3/hello-app.py
@@ -2,7 +2,9 @@ import time
 
 t_start = time.time()
 print('hello ..')
-while True:
+k=0
+while k<1000:
+  k=k+1
   end_t = time.time()
   if (end_t-t_start)%10 == 0:
     print(f'passe time : {end_t-t_start}')
